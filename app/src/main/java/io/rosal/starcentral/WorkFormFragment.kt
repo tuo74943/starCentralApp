aa1b2800746f7d4bd0733ab8075c3f14906b7dc8
tuo74943 <tuo74943@temple.edu>
2023-01-14 12:09:57 -0500

    minor changes to WorkFormFragment.kt

--- app/src/main/java/io/rosal/starcentral/WorkFormFragment.kt
+++ app/src/main/java/io/rosal/starcentral/WorkFormFragment.kt
@@ -1,5 +1,6 @@
 package io.rosal.starcentral
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,15 +11,20 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputLayout
+import java.util.*
 
 
 class WorkFormFragment : Fragment() {
 
     private lateinit var layout: View
     private lateinit var submitButton : Button
+    private lateinit var calanderButton: ImageButton
+    private lateinit var dateInputLayout : TextInputLayout
     lateinit var spinner : Spinner
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,6 +32,8 @@ class WorkFormFragment : Fragment() {
         layout = inflater.inflate(R.layout.fragment_work_form, container, false)
         submitButton = layout.findViewById(R.id.submitButton)
         spinner = layout.findViewById(R.id.spinner)
+        calanderButton = layout.findViewById(R.id.calandarImageButton)
+        dateInputLayout = layout.findViewById(R.id.dateInputLayout)
         return layout
     }
 
@@ -33,10 +41,24 @@ class WorkFormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupSpinner()
+        setupCalendarButton()
 
         submitButton.setOnClickListener {
             //TODO add to list here
+
+
             Navigation.findNavController(layout).navigate(R.id.action_workFormFragment_to_dashboardFragment)
+        }
+    }
+
+    private fun setupCalendarButton() {
+        val cal = Calendar.getInstance()
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+        val month = cal.get(Calendar.MONTH)
+        val year = cal.get(Calendar.YEAR)
+
+        calanderButton.setOnClickListener {
+
         }
     }
 
@@ -51,7 +73,6 @@ class WorkFormFragment : Fragment() {
             override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 val currentValue = parent?.getItemAtPosition(p2)
                 Log.d("WorkFormFrag", "$currentValue selected")
-
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
